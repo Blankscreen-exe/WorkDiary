02646048b862cdbdee873f6f60021becf25375d6
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-02-22 00:22:47 +0500

    added tool_description field

--- prototype/settings.py
+++ prototype/settings.py
@@ -3,5 +3,6 @@ settings = {
     "csv_dir": "logs",
     "all_records_file_name": 'work_log_all.csv',
     "datewise_records_prefix": 'work_log_',
-    "db_dir": "logs.db"
+    "db_dir": "logs.db",
+    "tool_description": "A commandline tool using built-in python tools for logging tasks at any moment"
 }
