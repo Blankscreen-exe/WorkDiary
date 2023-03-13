e9cbe85036510f9aca7b3ab329dcdd1a391fbbaf
Muhammad Hammad Hassan <blazinginpachi1850.0@gmail.com>
2023-03-13 08:17:52 -0400

    formatted code in colors.py

--- prototype/colors.py
+++ prototype/colors.py
@@ -1,12 +1,12 @@
 class colors:
- 
- 
+
     '''Colors class:reset all colors with colors.reset; two
     sub classes fg for foreground
     and bg for background; use as colors.subclass.colorname.
     i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable,
     underline, reverse, strike through,
     and invisible work with the main class i.e. colors.bold'''
+    
     reset = '\033[0m'
     bold = '\033[01m'
     disable = '\033[02m'
@@ -16,21 +16,21 @@ class colors:
     invisible = '\033[08m'
     
     class fg:
-            black = '\033[30m'
-            red = '\033[31m'
-            green = '\033[32m'
-            orange = '\033[33m'
-            blue = '\033[34m'
-            purple = '\033[35m'
-            cyan = '\033[36m'
-            lightgrey = '\033[37m'
-            darkgrey = '\033[90m'
-            lightred = '\033[91m'
-            lightgreen = '\033[92m'
-            yellow = '\033[93m'
-            lightblue = '\033[94m'
-            pink = '\033[95m'
-            lightcyan = '\033[96m'
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
     
     class bg:
         black = '\033[40m'
